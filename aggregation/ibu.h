6652546e55f4ece991d7b1b73f050ccb4db96217
galang48 <galangyudha1945@gmail.com>
2024-05-21 11:29:01 +0700

    member ibu

--- aggregation/ibu.h
+++ aggregation/ibu.h
@@ -3,5 +3,13 @@
 
 class ibu {
 public:
+	string nama;
+	vector<anak*> daftar_anak;
 
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
 };
